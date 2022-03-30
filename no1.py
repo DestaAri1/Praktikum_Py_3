6a5a12bb7ec66def0f953d548e5ec11ddaf47cd3
DestaAri1 <72483038+DestaAri1@users.noreply.github.com>
2022-03-30 22:01:31 +0700

    Add files via upload

---
+++ no1.py
@@ -0,0 +1,17 @@
+# Desta Ari Alfananda
+# 20051397008
+# 2020B
+
+class Stack(list):
+    push = list.append
+
+def modify_stack(symbolString):
+    stack=Stack()
+    result = []
+    for character in symbolString:
+        if character != '*':
+            stack.push(character)
+        else:
+            result.append(stack.pop())
+    return ''.join(result)
+print(modify_stack('EAS*Y*QUE***ST***IO*N***'))
